bb8f67a706d823c73597ff75b26e93c738468f7f
iulian-cmd <flysteelbird@gmail.com>
2021-11-06 11:19:23 +0100

    test commit

--- src/demenagementPack/Demenagement.java
+++ src/demenagementPack/Demenagement.java
@@ -1,26 +1,41 @@
 package demenagementPack;
+
 public class Demenagement {
-	 private String nom; // Attributes
-	 private int capCamion;
-	 private int noVoyage;
-	 private int emport;
-	 private int i;
-	 public Demenagement(String nom,int capCamion) { // Constructor
-	this.nom = nom;
-	this.capCamion = capCamion;
+	private String nom; // Attributes
+	private int capCamion;
+
+	public Demenagement(String nom, int capCamion) { // Constructor
+		this.nom = nom;
+		this.capCamion = capCamion;
 	}
-	 public void auTravail(int nbCartons){
-	  System.out.println(nom + " possède un camion de capacité: " + capCamion);
-	  System.out.println("Début du déménagement");
-	  System.out.println("noVoyage" + "nbCartons" + "emport" + "capCamion");
-	   while (i>capCamion) {
-		System.out.println(noVoyage + nbCartons + emport + capCamion);
-		if (nbCartons > capCamion) {
-			System.out.println(emport = 9);
+
+	public void auTravail(int nbCartons) {
+		
+		// initialisation des variable utilisés blabla
+		int nbVoyage = 0;
+		int emport = capCamion;
+		System.out.println(nom + " possède un camion de capacité: " + capCamion);
+		System.out.println("Il y a " + nbCartons + " de cartons a transporter.");
+		System.out.println("Début du déménagement");
+		System.out.println("__________________________________________________________________");
+		System.out.println(" N°Voyages \t | N°Cartons \t | Emport \t | CapacitéCamion");
+		System.out.println("__________________________________________________________________");
+		
+		// la boucle qui parcours les données
+		while (nbCartons > 0) {
+			// incrementer la valeur des nombres de voyage a chaque passage
+			nbVoyage++;
+			// condition si le nombre de cartons est plus grand que la capacité camion
+			if (nbCartons >= capCamion) {
+				// dans ce cas la on part avec le camion plein du coup l'emport est:
+				emport = capCamion;
+			} else 
+				emport = nbCartons;
+			// chaque fois qu'on fait un voyage la quantité est diminué par le nombre de cartons emportés
+			nbCartons = nbCartons - emport;
+			System.out.println(" " + nbVoyage + "\t \t | " + nbCartons + "\t \t | " + emport + "\t \t | " + capCamion);
 		}
-		else 
-			System.out.println(emport = nbCartons);
-	  }
-	  System.out.println("Fin du déménagement");
-	 }
+		System.out.println("__________________________________________________________________");
+		System.out.println("Fin du déménagement");
+	}
 }
--- src/demenagementPack/TestDem.java
+++ src/demenagementPack/TestDem.java
@@ -1,12 +1,23 @@
 package demenagementPack;
 
-public class TestDem{
-	 public static void main(String[] args) {
-	 System.out.println("Bienvenus");
-	 // on crée une "instance"
-	Demenagement dem = new Demenagement("Entreprisse bretonne",9);
-	 // on appelle la méthode d’instance "auTravail()
-	dem.auTravail(34);
-	 	  
+//Ceci importe la classe Scanner du package java.util
+import java.util.Scanner;
+
+public class TestDem {
+	public static void main(String[] args) {
+		System.out.println("Bienvenus");
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Veuillez saisir la capacité du camion: ");
+			int capCamion = sc.nextInt();
+//		System.out.println("Vous avez saisi le nombre : " + capCamion);
+			// on crée une "instance"
+			Demenagement dem = new Demenagement("Entreprisse bretonne", capCamion);
+			// on appelle la méthode d’instance "auTravail()
+			System.out.println("Veuillez saisir le nombre des cartons à transporter: ");
+			int nbCartons = sc.nextInt();
+//		System.out.println("Vous avez saisi le nombre : " + nbCartons);
+			dem.auTravail(nbCartons);
+		}
+
 	}
-	}
+}
